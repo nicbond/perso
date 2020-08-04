20502e3e0bee153e09b6359862da4ec4e2dbc612
nicbond <nicolasmartins78650@free.fr>
2020-08-04 10:17:26 +0200

    Treatment of data: without constraint validator and flush to the database

--- src/Service/Call.php
+++ src/Service/Call.php
@@ -2,9 +2,11 @@
 
 namespace App\Service;
 
+use App\Entity\Shop;
 use GuzzleHttp\Client;
 use JMS\Serializer\Serializer;
 use PSR\Log\LoggerInterface;
+use App\Repository\ShopRepository;
 
 class Call
 {
@@ -20,12 +22,17 @@ class Call
      * @var LoggerInterface
      */
     private $logger;
+    /**
+     * @var ShopRepository
+     */
+    private $repository;
 
-    public function __construct(Client $apiClient, Serializer $serializer, LoggerInterface $logger)
+    public function __construct(Client $apiClient, Serializer $serializer, LoggerInterface $logger, ShopRepository $repository)
     {
         $this->apiClient = $apiClient;
         $this->serializer = $serializer;
         $this->logger = $logger;
+        $this->repository = $repository;
     }
 
     public function getConnexion()
@@ -39,10 +46,48 @@ class Call
             return ['error' => 'Les informations ne sont pas disponibles pour le moment.'];
         }
         $httpCode = $response->getStatusCode();
-        var_dump($httpCode);die;
 
-        $data = $this->serializer->deserialize($response->getBody()->getContents(), 'array', 'json');
-        //var_dump($data);die;
+        if ($httpCode == 200) {
+            $data = $this->serializer->deserialize($response->getBody()->getContents(), 'array', 'json');
+            $this->getData($data);
+        }
+    }
+    
+    public function getData(array $data)
+    {
+        $size = count($data['data']); //20 résultats
+        $size = $size-1; // -1 à cause de l'indice 0
+        $i = 0;
+
+        do {
+            $id_shop = $data['data'][$i]['objectID'];
+            $shopSearch = $this->repository->findOneBy(array('id_shop' => $id_shop));
+
+                try {
+                    if (is_null($shopSearch)) {
+                        $shop = new Shop();
+                        $shop->setNameShop($data['data'][$i]['chain']);
+                        $shop->setAddress($data['data'][$i]['localisations'][0]['address']);
+                        $shop->setZipCode($data['data'][$i]['localisations'][0]['zipcode']);
+                        $shop->setCity($data['data'][$i]['localisations'][0]['city']);
+                        $shop->setImage($data['data'][$i]['picture_url']);
+                        $shop->setOffer($data['data'][$i]['offers'][0]['reduction']);
+                        $shop->setIdShop($data['data'][$i]['objectID']);
+                    } else {
+                        $shopAlreadyExist = $this->repository->find($shopSearch->getId());
+                        $shopAlreadyExist->setNameShop($data['data'][$i]['chain']);
+                        $shopAlreadyExist->setAddress($data['data'][$i]['localisations'][0]['address']);
+                        $shopAlreadyExist->setZipCode($data['data'][$i]['localisations'][0]['zipcode']);
+                        $shopAlreadyExist->setCity($data['data'][$i]['localisations'][0]['city']);
+                        $shopAlreadyExist->setImage($data['data'][$i]['picture_url']);
+                        $shopAlreadyExist->setOffer($data['data'][$i]['offers'][0]['reduction']);
+                        $shopAlreadyExist->setIdShop($data['data'][$i]['objectID']);
+                    }
+                } catch (\Doctrine\ORM\ORMException $e) {
+                    $errorMsg = 'Error Doctrine for the id_shop '.$data['data'][$i]['objectID'].'<br/>'.$e->getMessage();
+                }
+            $i++;
+        } while ($i <= $size);
     }
 }
 ?>
