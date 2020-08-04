507966834e3a7d7090a5ebac587fc44bc724e588
nicbond <nicolasmartins78650@free.fr>
2020-08-04 09:14:45 +0200

    Add ShopController with the configuration of the route with method GET

---
+++ src/Controller/ShopController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\Annotations\View;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ShopController extends AbstractFOSRestController
+{
+    /**
+     * @Rest\Get("les-habitues/shops", name="app_shops_list")
+     */
+    public function shop(Request $request, Call $call)
+    {
+
+    }
+}
