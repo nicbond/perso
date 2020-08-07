e5d019d2051b1e1d5f823cd06ff903907e864089
nicbond <nicolasmartins78650@free.fr>
2020-08-07 17:06:05 +0200

    Add update method

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -9,6 +9,9 @@ use FOS\RestBundle\Controller\Annotations\View;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\ConstraintViolationList;
+use App\Exception\ResourceValidationException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 use App\Service\Call;
 
@@ -23,6 +26,39 @@ class ShopController extends AbstractFOSRestController
     {
         $fisrt = $call->getConnexion();
         return $fisrt;
+    }
+
+    /**
+     * @Rest\View(StatusCode = 200)
+     * @Rest\Put(
+     *     path = "les-habitues/shops/{id}",
+     *     name = "app_shops_update",
+     *     requirements = {"id"="\d+"}
+     * )
+     * @ParamConverter("newShop", converter="fos_rest.request_body")
+     */
+    public function update(Shop $shop, Shop $newShop, ConstraintViolationList $violations)
+    {
+        if (count($violations)) {
+            $message = 'The JSON sent contains invalid data. Here are the errors you need to correct: ';
+            foreach ($violations as $violation) {
+                $message .= sprintf("Field %s: %s ", $violation->getPropertyPath(), $violation->getMessage());
+            }
+
+            throw new ResourceValidationException($message);
+        }
+
+        $shop->setNameShop($newShop->getNameShop());
+        $shop->setAddress($newShop->getAddress());
+        $shop->setZipCode($newShop->getZipCode());
+        $shop->setCity($newShop->getCity());
+        $shop->setImage($newShop->getImage());
+        $shop->setOffer($newShop->getOffer());
+        $shop->setIdShop($newShop->getIdShop());
+
+        $this->getDoctrine()->getManager()->flush();
+
+        return $shop;
     }
 
     /**
