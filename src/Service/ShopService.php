e56d2aafcdd9e8ace043cdd68c1bb28d294ebf5e
nicbond <nicolasmartins78650@free.fr>
2020-08-25 08:59:43 +0200

    Add a validator and complete the constructor

--- src/Service/ShopService.php
+++ src/Service/ShopService.php
@@ -3,17 +3,68 @@
 namespace App\Service;
 
 use App\Entity\Shop;
+use GuzzleHttp\Client;
+use PSR\Log\LoggerInterface;
+use JMS\Serializer\Serializer;
 use App\Repository\ShopRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 final class ShopService
 {
     /**
+     * @var Client
+     */
+    private $apiClient;
+    /**
+     * @var Serializer
+     */
+    private $serializer;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+    /**
      * @var ShopRepository
      */
     private $repository;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+    /**
+     * @var ValidatorInterface
+     */
+    private $validator;
 
-    public function __construct(ShopRepository $repository)
+    public function __construct(
+        Client $apiClient,
+        Serializer $serializer,
+        LoggerInterface $logger,
+        ShopRepository $repository,
+        EntityManagerInterface $entityManager,
+        ValidatorInterface $validator
+    )
     {
+        $this->apiClient = $apiClient;
+        $this->serializer = $serializer;
+        $this->logger = $logger;
         $this->repository = $repository;
+        $this->entityManager = $entityManager;
+        $this->validator = $validator;
+    }
+
+    public function validatorData(Shop $shop)
+    {
+        $violations = $this->validator->validate($shop);
+
+        if (count($violations)) {
+            $message = 'Invalid data. Here are the errors you need to correct: ' .'</br>';
+            foreach ($violations as $violation) {
+                $message .= sprintf("Field %s: %s ", $violation->getPropertyPath(), $violation->getMessage());
+                $message .= '</br>';
+            }
+            throw new \Exception($message);
+        }
     }
 }
